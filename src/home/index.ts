c5e82cc13ef401344beb2dc9d906e2133133b305
shivamsuyal <shivamsuyal25@gmail.com>
2023-06-22 18:16:24 +0530

    added home page

--- src/home/index.ts
+++ src/home/index.ts
@@ -1,1 +1,41 @@
 import "../css/home.css"
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import Glide from '@glidejs/glide'
+
+gsap.registerPlugin(ScrollTrigger)
+new Glide('.glide',{
+    type: 'carousel',
+    autoplay: 5000,
+    hoverpause: false,
+    animationDuration: 3000
+}).mount()
+
+const glideTimeline = gsap.timeline({
+    scrollTrigger:{
+        trigger:"#homePage",
+        start: "top top",
+        end: "bottom center",
+        scrub: 1,
+        // markers: true,
+        pin: true
+    },
+})
+
+glideTimeline
+    .to("nav",{
+        y:0
+    })
+    .to("#sideRectangle-wapper",{
+        x: 0
+    },"<")
+
+
+
+
+
+
+
+
+
+
